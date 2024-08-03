8c4b3ae375941afc1b7f4162df83df63b6915b57
Ratan Singh <ratan.web.dev@gmail.com>
2024-08-03 19:35:10 +0530

    Changes in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,54 +3,55 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <div id="logo">
-        <NavLink to="/">Widetran Language Services</NavLink>
-      </div>
-      <nav>
-        <div>
-          <NavLink
-            to="/"
-            style={({ isActive }) => ({
-              color: isActive ? "#893999" : "black",
-            })}
-          >
-            Home
-          </NavLink>
+    <div className="header-container">
+      <header>
+        <div id="logo">
+          <NavLink to="/">Widetran Language Services</NavLink>
         </div>
-        <div>
-          <NavLink
-            to="/about-us"
-            style={({ isActive }) => ({
-              color: isActive ? "#893999" : "black",
-            })}
-          >
-            About us
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/products"
-            style={({ isActive }) => ({
-              color: isActive ? "#893999" : "black",
-            })}
-          >
-            Products
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/contact"
-            style={({ isActive }) => ({
-              color: isActive ? "#893999" : "black",
-            })}
-          >
-            Contact
-          </NavLink>
-        </div>
-      </nav>
-      <div id="contact"></div>
-    </header>
+        <nav>
+          <div>
+            <NavLink
+              to="/"
+              style={({ isActive }) => ({
+                color: isActive ? "#893999" : "black",
+              })}
+            >
+              Home
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to="/about-us"
+              style={({ isActive }) => ({
+                color: isActive ? "#893999" : "black",
+              })}
+            >
+              About us
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to="/products"
+              style={({ isActive }) => ({
+                color: isActive ? "#893999" : "black",
+              })}
+            >
+              Products
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to="/contact"
+              style={({ isActive }) => ({
+                color: isActive ? "#893999" : "black",
+              })}
+            >
+              Contact
+            </NavLink>
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 };
 
