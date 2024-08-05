3855f89c60e4bb36a418dc90b32beba794954cb6
Ratan Singh <ratan.web.dev@gmail.com>
2024-08-05 23:56:41 +0530

    remove outline

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -9,20 +9,33 @@ const ScrollToTop = () => {
     const handleScroll = () => {
       setIsVisible(window.scrollY > 20);
     };
+
     window.addEventListener("scroll", handleScroll);
+
     handleScroll();
+
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  const scrollScreen = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    // const scrollDuration = 1;
+    // const scrollStep = -window.scrollY / (scrollDuration / 15);
+
+    // const scroll = () => {
+    //   if (window.scrollY !== 0) {
+    //     window.scrollBy(0, scrollStep);
+    //     requestAnimationFrame(scroll);
+    //   }
+    // };
+
+    // requestAnimationFrame(scroll);
+  };
 
   return (
     <>
       {isVisible && (
-        <div
-          className="scrollToTop"
-          onClick={() => {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }}
-        >
+        <div className="scrollToTop" onClick={scrollScreen}>
           <img src={top} alt="top" />
         </div>
       )}
