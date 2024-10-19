0271929fbac4b1b6883c67176eb8f974574e5f4a
Ratan Singh <ratan.web.dev@gmail.com>
2024-10-19 22:07:43 +0530

    styles

--- src/components/JoinHeader/index.js
+++ src/components/JoinHeader/index.js
@@ -4,8 +4,8 @@ const JoinHeader = () => {
   return (
     <div className="join-header">
       <p>
-        To join us, react us out at{" "}
-        <a href="tel:+917092177850">+91-7092177850</a> or mail us to{" "}
+        To join us, reach us at <a href="tel:+917092177850">+91-7092177850</a>{" "}
+        or mail us to{" "}
         <a href="mailto:ratandeepsingh702@gmail.com">
           ratandeepsingh702@gmail.com
         </a>
